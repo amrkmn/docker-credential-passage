ce1903bfc58df4d17ef35bdfbc0b486743fccc95
amrkmn <amartukiminj@gmail.com>
2026-01-30 08:46:49 +0000

    Improve dev version message

--- passage/cmd/main.go
+++ passage/cmd/main.go
@@ -5,6 +5,10 @@ import (
 	"github.com/amrkmn/docker-credential-passage/passage"
 )
 
+// Version is set at build time via ldflags
+var Version = "dev (unreleased)"
+
 func main() {
+	passage.SetVersion(Version)
 	credentials.Serve(passage.Passage{})
 }
